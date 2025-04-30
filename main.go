e47b792b2a4517d998fd80ef32203bb4f1dd2d7a
Burak Baykara <bubaykara@outlook.com>
2025-04-30 14:37:28 +0200

    Make custom method to register routes

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"go-cms/config"
-	"net/http"
+	"go-cms/routes"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,12 +10,6 @@ import (
 func main() {
 	config.LoadEnv()
 	r := gin.Default()
-
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Hello",
-		})
-	})
-
+	routes.RegisterRoutes(r)
 	r.Run(":" + config.GetEnv("PORT", "8080"))
 }
