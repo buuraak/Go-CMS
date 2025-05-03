087f14440a120856d763624eb1ce4796641acb38
Burak <bubaykara@outlook.com>
2025-05-03 22:46:21 +0200

    group routes into multiple functions

--- routes/routes.go
+++ routes/routes.go
@@ -7,8 +7,15 @@ import (
 )
 
 func RegisterRoutes(r *gin.Engine) {
-	api := r.Group("/api/v1")
+	registerHomeRoutes(r)
+	registerAPIRoutes(r)
+}
 
+func registerHomeRoutes(r *gin.Engine) {
 	r.GET("/", controllers.GetHome)
+}
+
+func registerAPIRoutes(r *gin.Engine) {
+	api := r.Group("/api/v1")
 	api.GET("/posts", controllers.GetPosts)
 }
