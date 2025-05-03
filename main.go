ab68b44d2a7ed4440dffaa4f8e670e39601fb5a6
Burak <bubaykara@outlook.com>
2025-05-03 13:57:41 +0200

    Add -seed flag to seed database

--- main.go
+++ main.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"flag"
 	"go-cms/config"
 	"go-cms/routes"
+	"go-cms/seeder"
+	"log"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	seed := flag.Bool("seed", false, "Run database seeder")
+	flag.Parse()
+
 	config.LoadEnv()
+
+	if *seed {
+		log.Println("Running database seeder...")
+		seeder.SeedDatabase()
+		return
+	}
+
 	r := gin.Default()
 	routes.RegisterRoutes(r)
 	r.Run(":" + config.GetEnv("PORT", "8080"))
