130fc4a5df47502cb2631a28850438281b23b1dc
Burak <bubaykara@outlook.com>
2025-05-04 15:41:40 +0200

    route changes

--- routes/routes.go
+++ routes/routes.go
@@ -4,18 +4,26 @@ import (
 	"go-cms/controllers"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
-func RegisterRoutes(r *gin.Engine) {
+func RegisterRoutes(r *gin.Engine, db *gorm.DB) {
 	registerHomeRoutes(r)
-	registerAPIRoutes(r)
+	registerAPIRoutes(r, db)
 }
 
 func registerHomeRoutes(r *gin.Engine) {
 	r.GET("/", controllers.GetHome)
 }
 
-func registerAPIRoutes(r *gin.Engine) {
+func registerAPIRoutes(r *gin.Engine, db *gorm.DB) {
+
 	api := r.Group("/api/v1")
+	// post endpoints
 	api.GET("/posts", controllers.GetPosts)
+
+	// User endpoints
+	api.GET("/users/:user", func(c *gin.Context) {
+		controllers.GetUser(c, db)
+	})
 }
