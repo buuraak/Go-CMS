337e8ddde29650b6edd4cffa651b25b0980e6f23
Burak <bubaykara@outlook.com>
2025-05-04 15:31:01 +0200

    Add first name, last name to user and omit password from json

--- models/user.go
+++ models/user.go
@@ -6,7 +6,9 @@ type User struct {
 	gorm.Model
 	Username   string `gorm:"unique;not null"`
 	Email      string `gorm:"unique;not null"`
-	Password   string `gorm:"not null"`
+	FirstName  string `gorm:"not null"`
+	LastName   string `gorm:"not null"`
+	Password   string `gorm:"not null" json:"-"`
 	Role       string `gorm:"type:enum('admin','editor');default:'editor';not null"`
 	CreatedBy  uint   `gorm:"not null"`
 	IsVerified bool   `gorm:"default:false;not null"`
