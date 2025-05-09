31d7779b34ee50285b8819e9441dee4b9b3a248f
Burak <bubaykara@outlook.com>
2025-05-09 15:50:53 +0200

    Create login method

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,11 +1,14 @@
 package controllers
 
 import (
+	"go-cms/config"
 	"go-cms/models"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
 	"gorm.io/gorm"
 )
 
@@ -22,6 +25,14 @@ func GetPosts(c *gin.Context) {
 }
 
 func GetUser(c *gin.Context, db *gorm.DB) {
+	claims, _ := c.Get("claims")
+	userClaims := claims.(jwt.MapClaims)
+
+	if userClaims["role"] != "admin" {
+		c.JSON(http.StatusForbidden, gin.H{"error": "Acces denied"})
+		return
+	}
+
 	uid := c.Param("user")
 	user := models.User{}
 	err := db.Where("id = ? OR username = ? OR email = ?", uid, uid, uid).First(&user).Error
@@ -35,4 +46,42 @@ func GetUser(c *gin.Context, db *gorm.DB) {
 		return
 	}
 	c.JSON(http.StatusOK, user)
+}
+
+func Login(c *gin.Context, db *gorm.DB) {
+	var input struct {
+		Username string `json:"username" binding:"required"`
+		Password string `json:"password" binding:"required"`
+	}
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid username or password"})
+		return
+	}
+
+	var user models.User
+	if err := db.Where("username = ?", input.Username).First(&user).Error; err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid username or password"})
+		return
+	}
+
+	if user.Password != input.Password {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid username or password"})
+		return
+	}
+
+	secret := config.GetJWTSecret()
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"id":       user.ID,
+		"username": user.Username,
+		"role":     user.Role,
+		"exp":      time.Now().Add(time.Hour).Unix(),
+	})
+
+	tokenString, err := token.SignedString([]byte(secret))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to generate token"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"token": tokenString})
 }
