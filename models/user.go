52cf8c50639c08d48812921821d8ef4af7e9f5b1
Burak <bubaykara@outlook.com>
2025-05-09 15:50:26 +0200

    add role customer

--- models/user.go
+++ models/user.go
@@ -9,7 +9,7 @@ type User struct {
 	FirstName  string `gorm:"not null"`
 	LastName   string `gorm:"not null"`
 	Password   string `gorm:"not null" json:"-"`
-	Role       string `gorm:"type:enum('admin','editor');default:'editor';not null"`
+	Role       string `gorm:"type:enum('admin','editor','customer');default:'customer';not null"`
 	CreatedBy  uint   `gorm:"not null"`
 	IsVerified bool   `gorm:"default:false;not null"`
 }
