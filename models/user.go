257dc4d65201e21d10d6512eecf4a8d9b31efc25
Burak <bubaykara@outlook.com>
2025-05-09 21:18:27 +0200

    Update User model

--- models/user.go
+++ models/user.go
@@ -4,12 +4,13 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Username   string `gorm:"unique;not null"`
-	Email      string `gorm:"unique;not null"`
-	FirstName  string `gorm:"not null"`
-	LastName   string `gorm:"not null"`
-	Password   string `gorm:"not null" json:"-"`
-	Role       string `gorm:"type:enum('admin','editor','customer');default:'customer';not null"`
-	CreatedBy  uint   `gorm:"not null"`
-	IsVerified bool   `gorm:"default:false;not null"`
+	ID                uint   `gorm:"primaryKey"`
+	Username          string `gorm:"unique;not null"`
+	Email             string `gorm:"unique;not null"`
+	FirstName         string `gorm:"not null"`
+	LastName          string `gorm:"not null"`
+	Password          string `gorm:"not null" json:"-"`
+	Role              string `gorm:"type:enum('admin','editor','customer');default:'customer';not null"`
+	IsVerified        bool   `gorm:"default:false;not null"`
+	VerificationToken string
 }
